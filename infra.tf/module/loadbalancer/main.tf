fd4a8435ca03c7c5b86e8d049ba3252b8949a725
sagar sonule <122080557+sonulesagar@users.noreply.github.com>
2023-03-18 16:04:59 +0530

    Update main.tf

--- infra.tf/module/loadbalancer/main.tf
+++ infra.tf/module/loadbalancer/main.tf
@@ -7,6 +7,12 @@ resource "aws_lb" "alb" {
   subnets            = var.subnets
   enable_deletion_protection = false
   
+  access_logs {
+    bucket =aws_s3_bucket.my-bucket.id
+    prefix =var.appname
+    enabled = true    
+  }
+  
 tags = merge (var.tags,{Name= format ("%s-%s-%s",var.appname,var.env,"application")})
  }
 
@@ -19,4 +25,33 @@ resource "aws_lb" "nlb" {
   enable_deletion_protection = false
    
 tags = merge (var.tags,{Name= format ("%s-%s-%s",var.appname,var.env,"network")})
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "mybucket-${var.appname}-${var.env}-${random_string.random.id}"
+}
+
+resource "random_string" "random" {
+  length           = 5
+  special          = false
+  upper            = false
+}
+
+resource "aws_s3_bucket_policy" "log-bucket-policy" {
+  bucket = aws_s3_bucket.my-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.my-bucket.arn}/*"
+      }
+    ]
+  })
 }
